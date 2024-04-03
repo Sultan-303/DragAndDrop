f9bcf011641b8608a7fe51e08f267ef342aac4ea
Sultan <531213@student.fontys.nl>
2024-04-03 11:50:10 +0200

    Functional upload + draganddrop

--- DragAndDrop/dragdroptutorial/obj/Release/net8.0/dragdroptutorial.AssemblyInfo.cs
+++ DragAndDrop/dragdroptutorial/obj/Release/net8.0/dragdroptutorial.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("dragdroptutorial")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+697ade8a343ddf145cc724ea8dad8e5c484f60fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("dragdroptutorial")]
